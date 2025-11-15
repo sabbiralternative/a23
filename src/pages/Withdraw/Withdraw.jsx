eda3a1440a434d944bcbb29717ac9cc02818d9b3
sabbirHossain <sabbirshnt@gmail.com>
2025-11-15 11:19:07 +0600

    done

--- src/pages/Withdraw/Withdraw.jsx
+++ src/pages/Withdraw/Withdraw.jsx
@@ -1,69 +1,17 @@
-import { useEffect, useState } from "react";
-import SelectAmount from "./SelectAmount";
-import BankAccounts from "./BankAccounts";
-import useBankAccount from "../../hooks/useBankAccount";
-import WithdrawConfirm from "./WithdrawConfirm";
-import AddBank from "../../components/modal/bank/AddBank";
-import useContextState from "../../hooks/useContextState";
+import { useState } from "react";
+import ChooseAmount from "./ChoseAmount";
+import BankAccount from "./BankAccount";
 
 const Withdraw = () => {
   const [amount, setAmount] = useState("");
-  const [showBankAccount, setShowBankAccount] = useState(false);
-  const [confirmWithdraw, setConfirmWithdraw] = useState(false);
-  const [bank, setBank] = useState("");
-  const { addBank, setAddBank } = useContextState();
-  const payload = {
-    type: "getBankAccounts",
-    status: "1",
-  };
-  const { bankData, refetchBankData } = useBankAccount(payload);
-
-  useEffect(() => {
-    setShowBankAccount(false);
-    setConfirmWithdraw(false);
-  }, []);
-
-  useEffect(() => {
-    if (showBankAccount && bankData?.length < 1) {
-      setShowBankAccount(false);
-      setAddBank(true);
-    }
-  }, [bankData, setAddBank, showBankAccount]);
-
+  const [tab, setTab] = useState("choseAmount");
   return (
-    <>
-      {!showBankAccount && !confirmWithdraw && (
-        <SelectAmount
-          setShowBankAccount={setShowBankAccount}
-          setAmount={setAmount}
-          amount={amount}
-        />
+    <div className="dep-w-info-bc  ng-star-inserted">
+      {tab === "choseAmount" && (
+        <ChooseAmount amount={amount} setAmount={setAmount} setTab={setTab} />
       )}
-      {showBankAccount && bankData?.length > 0 && (
-        <BankAccounts
-          refetchBankData={refetchBankData}
-          setAmount={setAmount}
-          bankData={bankData}
-          setConfirmWithdraw={setConfirmWithdraw}
-          setShowBankAccount={setShowBankAccount}
-          bank={bank}
-          setBank={setBank}
-        />
-      )}
-      {addBank && bankData?.length < 1 && (
-        <AddBank setAddBank={setAddBank} refetchBankData={refetchBankData} />
-      )}
-      {confirmWithdraw && (
-        <WithdrawConfirm
-          amount={amount}
-          bank={bank}
-          setAmount={setAmount}
-          setShowBankAccount={setShowBankAccount}
-          setConfirmWithdraw={setConfirmWithdraw}
-          setBank={setBank}
-        />
-      )}
-    </>
+      {tab === "bank" && <BankAccount amount={amount} />}
+    </div>
   );
 };
 
