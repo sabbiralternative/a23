e879f4ea85c3db924d69e174789eb7a13104c5eb
sabbirHossain <sabbirshnt@gmail.com>
2026-02-25 14:11:19 +0600

    done

--- src/pages/IFrame/IFrame.jsx
+++ src/pages/IFrame/IFrame.jsx
@@ -19,7 +19,7 @@ const IFrame = () => {
         gameId: gameId,
         isHome: false,
         mobileOnly: true,
-        casino_currency: Settings.casino_currency,
+        casinoCurrency: Settings.casino_currency,
       };
 
       try {
