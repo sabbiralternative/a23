0c6d93b91944726f398a08fd5a122d170fe1c4c0
sabbirHossain <sabbirshnt@gmail.com>
2026-04-07 14:22:52 +0600

    done

--- src/components/modal/BetSlip.jsx
+++ src/components/modal/BetSlip.jsx
@@ -10,6 +10,7 @@ import useLanguage from "../../hooks/useLanguage";
 import { v4 as uuidv4 } from "uuid";
 import { useParams } from "react-router-dom";
 import { AxiosJSEncrypt } from "../../lib/AxiosJSEncrypt";
+import { isBetDelay, isDelay } from "../../utils/isBetDelay";
 
 const BetSlip = ({
   setOpenBetSlip,
@@ -108,21 +109,14 @@ const BetSlip = ({
         ...payload,
 
         nounce: uuidv4(),
-        isbetDelay:
-          placeBetValues?.btype === "FANCY" &&
-          placeBetValues?.eventTypeId === "4"
-            ? false
-            : Settings.bet_delay,
+        isbetDelay: isBetDelay(placeBetValues),
         apk: closePopupForForever ? true : false,
       },
     ];
     let delay = 0;
-    if (
-      placeBetValues?.btype !== "FANCY" &&
-      placeBetValues?.eventTypeId !== "4"
-    ) {
+    if (isDelay(placeBetValues)) {
       if (
-        (eventTypeId == 4 || eventTypeId == 2) &&
+        eventTypeId == 4 &&
         placeBetValues?.btype === "MATCH_ODDS" &&
         price > 3 &&
         placeBetValues?.name?.length === 2
@@ -131,7 +125,7 @@ const BetSlip = ({
       }
 
       if (
-        (eventTypeId == 4 || eventTypeId == 2) &&
+        eventTypeId == 4 &&
         placeBetValues?.btype === "MATCH_ODDS" &&
         price > 7 &&
         placeBetValues?.name?.length === 3
